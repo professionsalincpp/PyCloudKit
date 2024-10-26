7931b05bb01e0dbe27e6263662afc1b9f50b6013
griuc <griguchaev@gmail.com>
2024-10-26 17:13:16 +0300

    Release

--- main.py
+++
@@ -1,9 +0,0 @@
-from pycloudkit import CloudServer
-
-server = CloudServer('127.0.0.1', 8080, 'databases/cloud.db')
-              
-def main():
-    server.start()
-
-if __name__ == '__main__':
-    main()
